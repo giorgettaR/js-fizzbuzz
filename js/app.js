2e8b4ba7a5e768694c663d4da0cb0be98fa513cc
GiorgettaR <giorgetta.roberto@gmail.com>
2024-03-06 16:59:38 +0100

    FIX: JS - rimozione condizione annidata

--- js/app.js
+++ js/app.js
@@ -14,30 +14,28 @@ for (let c = 0; c < 100; c++) {
     squareElement.classList.add('col-1','chessSquare')
         // console.log(squareElement);
     
-    if (n % 3 == 0) {
-        if (n % 5 == 0) {
-            // customizzazione cella multipli di 3 e 5
-            squareElement.innerHTML = '<div>fizz buzz</div>';
-            squareElement.classList.add('fizzBuzz')
-            console.log('fizz buzz');
-        } else {
-            // customizzazione cella multipli di 3
-            squareElement.innerHTML = '<div>fizz</div>';
-            squareElement.classList.add('fizz')
-            console.log('fizz');
-        }
-    } else if (n % 5 == 0) {
+    if (n % 3 == 0 && n % 5 == 0) {
+        // customizzazione cella multipli di 3 e 5
+        squareElement.innerHTML = '<div>fizz buzz</div>';
+        squareElement.classList.add('fizzBuzz')
+        console.log('fizz buzz');
+        } else if (n % 3 == 0)  {
+        // customizzazione cella multipli di 3
+        squareElement.innerHTML = '<div>fizz</div>';
+        squareElement.classList.add('fizz')
+        console.log('fizz');
+        } else if (n % 5 == 0) {
         // customizzazione cella multipli di 5
         squareElement.innerHTML = '<div>buzz</div>';
         squareElement.classList.add('buzz')
         console.log('buzz');
-    } else {
+        } else {
         // customizzazione cella semplice
         squareElement.innerHTML = '<div>' + n + '</div>';
         console.log(n);
-    }
+        }
 
-    // aggiungere cella a file html
-    chessBoardElement.append(squareElement);
+        // aggiungere cella a file html
+        chessBoardElement.append(squareElement);
 
 }
